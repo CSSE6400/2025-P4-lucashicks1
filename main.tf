8af1827f2495a9ae2b3f1441f245f56a5ff590ad
lucashicks1 <lucasjhicks1@gmail.com>
2025-03-25 09:43:28 +1000

    fix: Follows tut for ingress and egress rules.

--- main.tf
+++ main.tf
@@ -56,27 +56,47 @@ resource "aws_instance" "hextris-server" {
 resource "aws_security_group" "hextris-server" {
   name        = "hextris-server"
   description = "Hextris HTTP and SSH access"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.hextris-server.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+#   security_group_id = aws_security_group.hextris-server.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.hextris-server.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  to_port           = 22
-  ip_protocol       = "TCP"
-}
+# resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+#   security_group_id = aws_security_group.hextris-server.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   from_port         = 22
+#   to_port           = 22
+#   ip_protocol       = "TCP"
+# }
 
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
-  security_group_id = aws_security_group.hextris-server.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1"
-}
+# resource "aws_vpc_security_group_egress_rule" "allow_all_traffic" {
+#   security_group_id = aws_security_group.hextris-server.id
+#   cidr_ipv4         = "0.0.0.0/0"
+#   ip_protocol       = "-1"
+# }
 
 output "hextris-url" {
   value = aws_instance.hextris-server.public_ip
